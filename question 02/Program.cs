951698c48478b979a86ada912f6da9ba4ff484e3
Veru Satharasingha <Veru Satharasingha@DESKTOP-BN34K29>
2024-01-28 19:30:14 +0530

    Add project files.

---
+++ question 02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace question02
+{
+    class question02
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("enter a number");
+                int number = int.Parse(Console.ReadLine());
+
+                if (isEven(number))
+                {
+                    Console.WriteLine("even");
+                }
+                else
+                {
+
+                    Console.WriteLine("odd");
+                }
+
+            }
+        }
+
+        static bool isEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
+
